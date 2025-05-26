69b329fc198b55045388fed07dc8189af199876b
Habtin <abtin.hidaji.2013@gmail.com>
2025-05-26 18:55:16 +0100

    Added mapper function

--- mapreduce_flight_counter.py
+++ mapreduce_flight_counter.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import csv
+from collections import defaultdict
+from multiprocessing import Pool, cpu_count
+
+def mapper(record):
+    """
+    Simulates the Map step: returns (passenger_id, 1) for a single record.
+    """
+    try:
+        fields = record.strip().split(",")
+        passenger_id = fields[0]
+        return (passenger_id, 1)
+    except IndexError:
+        return None
+
